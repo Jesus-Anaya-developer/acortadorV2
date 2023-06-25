c8556ada86b8b91b3503353ad22726135c0c5743
Jesus Anaya <jesus.anaya.developer@gmail.com>
2023-06-24 21:06:28 -0600

    importacion de HttpClientModule

--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -5,5 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class ShortUrlService {
 
-  constructor() { }
+  private URL: string;
+  private TOKEN: string;
+
+  constructor() {
+    this.URL = "https://api-ssl.bitly.com/v4/shorten";
+    this.TOKEN = "b49b991d3d226435b5975d37ad3de5570fbd7f45";
+  }
 }
